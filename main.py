6ce673b6fb136c958821d8d96022d35c5c703327
Kitaek <foxhound2234@gmail.com>
2021-03-10 18:33:03 +0200

    adjusted the main.py to return a HTML, from default JSON

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ app = fastapi.FastAPI()
 
 @app.get('/')
 def index():
-    return {
-        'message': "Hello world"
-    }
+    content = """
+    
+    <h1> Fast API with HTML response </h1>
+    
+    <div> This page is in HTML </div>
+    
+    """
+    response = fastapi.responses.HTMLResponse(content)
+    return response
+        # initial JSON return:
+    # 'message': "Hello world"
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
